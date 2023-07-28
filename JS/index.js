f56672b29f041935e9bdb1f42135a7f064417716
Shamsmedhat <125091923+Shamsmedhat@users.noreply.github.com>
2023-07-28 22:18:34 +0300

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-var storedUsers = JSON.parse(localStorage.getItem("allUsers"));
+var storedUsers = JSON.parse(localStorage.getItem("allUsers")) || [];
 var enterEmailInp = document.getElementById("enterEmailInp");
 var enterPasswordInp = document.getElementById("enterPasswordInp");
 var logInBtn = document.getElementsByClassName("logInBtn");
@@ -6,17 +6,21 @@ var warningMsg = document.getElementsByClassName("warningMsg");
 var welcomName = document.querySelector(".welcome h1");
 
 var currentUser = logInBtn[0].addEventListener("click", function () {
-  var foundUser = storedUsers.find(
-    (user) =>
-      user.email === enterEmailInp.value &&
-      user.password === enterPasswordInp.value
-  );
+  if (storedUsers) {
+    var foundUser = storedUsers.find(
+      (user) =>
+        user.email === enterEmailInp.value &&
+        user.password === enterPasswordInp.value
+    );
 
-  if (foundUser) {
-    localStorage.setItem("currentUser", JSON.stringify(foundUser));
-    location.assign("home.html");
+    if (foundUser) {
+      localStorage.setItem("currentUser", JSON.stringify(foundUser));
+      location.assign("home.html");
+    } else {
+      warningMsg[0].innerHTML = "Invalid email or password. Please try again.";
+    }
   } else {
-    warningMsg[0].innerHTML = "Invalid email or password. Please try again.";
+    warningMsg[0].innerHTML = "No users found. Please sign up first.";
   }
 
   clearInpsSignIn();
